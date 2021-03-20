3d0d901b0f40fe6d0ae69ae47c8db5cc0d8dfee4
norahBOX <parksso0530@gmail.com>
2021-03-20 14:22:31 +0900

    username 컬럼 삭제, slug로 대체

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :phone, :instagram, :twitter])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :phone, :instagram, :twitter])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :slug, :phone, :instagram, :twitter])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :slug, :phone, :instagram, :twitter])
   end
 
 end
