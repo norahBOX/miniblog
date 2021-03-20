815af0f8c2276ed981d2e73ef87b000521b375fd
norahBOX <parksso0530@gmail.com>
2021-03-20 15:30:42 +0900

    포스트 페이지 index, 권한 설정
    - 로그인한 유저가 자기 페이지에만 글을 쓰고 수정할 수 있는 메소드 추가
    - 인덱스페이지 추가

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
-  
+  before_action :authenticate_user!, only: %I[new create edit update destroy] #로그인 한 유저만 글을 쓸 수 있다? 작가 인증 받은 사람이 아니고?
+  before_action :set_post, only: %I[show edit update]
+  before_action :set_post_writer
+
+  def index
+    @posts = @user.posts
+  end
+
   def new
 
   end
@@ -10,7 +17,31 @@ class PostsController < ApplicationController
 
   private
   def set_post
-    
+    # @posts = Post.
+    @post = Post.find(params[:id])
+  end
+
+  # def post_params
+  # end
+
+  def post_writer_params
+    params[:user_slug]
+  end
+
+  def set_post_writer # 현 페이지 블로그 소유자
+    post_writer_params
+    @user = User.friendly.find(post_writer_params)
+  end
+
+  def verify_user_own # 현 페이지 블로그 소유자가 로그인한 사람인가요?
+    user_signed_in? && current_user.friendly_id == @user.friendly_id
+  end
+
+  def redirect_unverified_user
+    unless verify_writer_own
+      flash[:alert] = "팀만 이 스터디방에 글을 작성할 수 있습니다!"
+      redirect_to root_path
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  extend FriendlyId
+  friendly_id :slug, use: :slugged
 
   has_many :posts
 
