e859e830a9809372632f645b80667208e6d0a8e5
norahBOX <parksso0530@gmail.com>
2021-03-20 18:22:28 +0900

    post 쓰기 기능 등 틀거리 잡기 완료

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,35 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: %I[new create edit update destroy] #로그인 한 유저만 글을 쓸 수 있다? 작가 인증 받은 사람이 아니고?
-  before_action :set_post, only: %I[show edit update]
   before_action :set_post_writer
+  before_action :set_post, only: %I[show edit update]
 
   def index
     @posts = @user.posts
   end
 
   def new
-
+    @post = Post.new
   end
+  
+  def create
+    @post = @user.posts.new(post_params)
+    @post.save
+
+    redirect_to user_post_path(@user.slug, @post)
+  end
+
 
   def show
 
   end
+
+  def edit
+  end
+
+  def update 
+  end
+
+
 
   private
   def set_post
@@ -21,16 +37,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-  # def post_params
-  # end
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 
   def post_writer_params
     params[:user_slug]
   end
 
   def set_post_writer # 현 페이지 블로그 소유자
-    post_writer_params
-    @user = User.friendly.find(post_writer_params)
+    @user = User.friendly.find(params[:user_slug])
   end
 
   def verify_user_own # 현 페이지 블로그 소유자가 로그인한 사람인가요?
